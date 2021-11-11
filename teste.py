04ee70ebb4d54c70f417f12be28bcf3be074be62
Allan <allansilvaoliveira@outlook.com>
2021-11-10 22:44:53 -0300

    att 1

--- teste.py
+++ teste.py
@@ -14,36 +14,66 @@ lista_ecoponto = ["Ecoponto Região Central (Rua Francisco Theodoro, 1050, Vila 
 # lista com o calculo do usuário com as lista de latitude e longitude
 lista_dist = []
 
-def menu():
-    print("----MENU----")
-    print("(1) - Materiais")
-    print("(2) - Ecopontos")
-    print("(3) - Endereço mais próximo")
-    print("(4) - Pontos Negativos")
-menu()
+def menu_initial():
+    
+    print('''     -------------MENU-------------------
+    |  (1) - Materiais                   |
+    |  (2) - Ecopontos                   |
+    |  (3) - Endereço mais próximo       |
+    |  (4) - Pontos Negativos            |
+    |  (5) - Sair                        |
+     ------------------------------------
+    ''')
+    
+    
+    
+menu_initial()
 
 opcao = int(input("Digite a sua escolha: "))
 
+    
 if opcao == 1:
     
-    funcao =int(input("Escolha um desses materiais \n((1) Metal, (2) Plástico, (3) Eletrônico, (4) Baterias, (5)  Vidro): "))
+    def menu_mat():
+        
+        print('''
+        ------ Materiais -------
+        |  (1) Metal           |
+        |  (2) Plástico        |
+        |  (3) Eletrônico      |
+        |  (4) Baterias        |
+        |  (5) Vidros          |
+         ----------------------
+        ''')
+
+    menu_mat()
+
+    funcao = int(input("Escolha o material desejado: "))
+
     
     if funcao == 1:
         print("Algumas das principais características dos metais é a alta capacidade para conduzir calor e eletricidade e quando polidos eles ganham um brilho característico, também possuindo alta densidade eles podem ser usados como reforço para estruturas e como utensílios para cozinha ou como ferramentas para obras. Metais também são formadores de cátions que são moléculas com o número de cátions maior que o de ânions.")
-
-    if funcao == 2:
+        
+    
+    elif funcao == 2:
         print("O plástico está presente em quase todos os lugares do mundo moderno, sendo criado no final do século xx e tendo seu nome derivado do grego que significa aquilo que pode ser moldado sendo barato e de fácil manuseio esse material foi ganhando espaço durante os anos sendo criado a partir da resina do petróleo e pertencendo ao grupo dos polimeros também existem plásticos que são derivados de plantas como do algodão porém sua produção é mais complexa resultando em uma produção em menor escala")
 
-    if funcao == 3:
+    elif funcao == 3:
         print("Na reciclagem do lixo eletrônico nem tudo que está la pode ser considerado descartável pois dentro de cada eletrônico ainda existem componentes que podem ser reciclados esses componentes que ainda podem ser reciclados são enviados para a industria onde são derretidos e transformados em matéria prima novamente o lixo eletrônico é um amontoado de todos os outros materiais oque acaba dificultando em sua reciclagem.")
 
-    if funcao == 4:
+    elif funcao == 4:
         print("As baterias como as pilhas por exemplo são formadas por dois eletrodos e um eletrolito e suas matéias primas são zinco metálico, pasta de cloreto de amonio, água, dióxido de manganês e grafita tendo a capacidade de transformar a energia quimica em energia eletrica ja a bateria de carro precisa de chumbo, ácido sulfúrico e plástico ja para o ativo da bateria são usados oxidos de chumbo em pó, agua e dependendo da sua carga será adicionado aditivos diferentes.")
-    
-    if funcao == 5:
-        print("O vidro é um dos poucos materiais que podem ser 100% reciclados não perdendo nada no processo, existe também um tipo de vidro chamado de insulado que é capaz de impedir o som e o calor de entrar no ambiente sem perder a luminosidade no processo, em fachadas de estruturas os tipos de vidro que devem ser usados são laminados, aramados e insulados para a fabricação do vidro são usados areia, sódio, cálcio e magnésio e todos eles são misturados em um forno podendo chegar até temperaturas de 1600°C.")
 
-if opcao == 2:
+    elif funcao == 5:
+        print("O vidro é um dos poucos materiais que podem ser 100% reciclados não perdendo nada no processo, existe também um tipo de vidro chamado de insulado que é capaz de impedir o som e o calor de entrar no ambiente sem perder a luminosidade no processo, em fachadas de estruturas os tipos de vidro que devem ser usados são laminados, aramados e insulados para a fabricação do vidro são usados areia, sódio, cálcio e magnésio e todos eles são misturados em um forno podendo chegar até temperaturas de 1600°C.")
+    
+    else:
+        funcao != "1" and funcao != "2" and funcao != "3" and funcao != "4" and funcao != "5"
+        print("Opção inválida!!")
+
+        
+
+elif opcao == 2:
     print("- Ecoponto Região Central (Rua Francisco Theodoro, 1050, Vila Industrial)")
     print("- Ecoponto Parque Itajaí (Rua Celso Soares Couto)")
     print("- Ecoponto Parque São Jorge  (R. Plácida Pretini, 196-270 - Parque São Jorge, Campinas - SP, 13064-812)")
@@ -52,7 +82,7 @@ if opcao == 2:
     print("- Ecoponto Vida Nova (R. Lídia Martins de Assis - Conj. Hab. Vida Nova, Campinas - SP, 13057-558)")
     print("- Ecoponto Jardim Eulina (Av. Mal. Rondon, 2296-2382 - Jardim Chapadão, Campinas - SP, 13063-490)")
 
-if opcao == 3:
+elif opcao == 3:
     endereco = pycep_correios.get_address_from_cep(str(input("Insira seu CEP: \n(Exemplo 0000-0000)" )))
     geolocator = Nominatim(user_agent="APP")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
@@ -70,7 +100,7 @@ if opcao == 3:
 
     print("O local de descarte mais próximo é:", lista_ecoponto[posicao])
 
-if opcao == 4:
+elif opcao == 4:
     print("Malefícios")
     print("----------------------------")
     print("Escreva o nome do material desejado \n( Metal, Plástico, Eletrônico, Baterias, Vidros)")
@@ -99,4 +129,13 @@ if opcao == 4:
     
     if funcao1 == "vidros":
         print("----------- VIDROS -----------")
-        print("Em períodos onde há mais chuvas, como o verão, o risco da leptospirose aumenta, pois há maior\n probabilidade de haver enchentes, inundações e, consequentemente, maior contato com o transmissor da doença.\n A leptospirose é uma doença grave, que pode levar até a morte,\n mas que pode ser tratada”, afirma Tânia.")
+        print("Em períodos onde há mais chuvas, como o verão, o risco da leptospirose aumenta, pois há maior\n probabilidade de haver enchentes, inundações e, consequentemente, maior contato com o transmissor da doença.\n A leptospirose é uma doença grave, que pode levar até a morte,\n mas que pode ser tratada.")
+
+elif opcao == 5:
+    print("Obrigado por utilizar nosso APP!!")
+    exit()
+
+else:
+    opcao != "1" and opcao != "2" and opcao != "3" and opcao != "4" and opcao != "5"
+    print("Opção inválida!!")
+    
